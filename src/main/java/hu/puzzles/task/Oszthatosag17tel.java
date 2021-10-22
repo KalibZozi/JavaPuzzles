0938deabc7d86364083d5e1adf88dd63a8cffc2c
Petrik Vilmos <vili528@gmail.com>
2021-10-22 13:27:23 +0200

    Vili kész 17 oszthatóság

--- src/main/java/hu/puzzles/task/Oszthatosag17tel.java
+++ src/main/java/hu/puzzles/task/Oszthatosag17tel.java
@@ -8,10 +8,46 @@ import java.util.List;
  * Oszthatóság 17-el
  */
 public class Oszthatosag17tel {
+    public String kozbulsoSzamok="";
 
     public List<String> run(List<String> bemenet) {
         List<String> kimenet = new ArrayList<>();
+        String bemenetiSzam=bemenet.get(0);
+        if(Integer.parseInt(bemenetiSzam)%17==0){
+            kimenet.add("IGEN");
+        }else{
+            kimenet.add("NEM");
+        }
+        osztas17(Integer.parseInt(bemenetiSzam));
 
+        //System.out.println(kivonas(5));
+
+        kimenet.add(kozbulsoSzamok);
         return kimenet;
     }
+    public void osztas17(int bemenetiSzam){
+        int vegeSzam=bemenetiSzam%10;
+        int elejeSzam=bemenetiSzam/10;
+        int ujszam=elejeSzam-(vegeSzam*5);
+        if(ujszam>=0) {
+            kozbulsoSzamok += ujszam + " ";
+        }
+        if(ujszam<=0){
+            if(kozbulsoSzamok.length()>0){
+               kozbulsoSzamok= kozbulsoSzamok.substring(0,kozbulsoSzamok.length()-1);
+            }
+            return;
+        }
+        osztas17(ujszam);
+    }
+
+    public int kivonas(int ertek) {
+        int visszaSzam = --ertek;
+        if (visszaSzam <= 0) {
+            return visszaSzam;
+        }
+        return kivonas(visszaSzam);
+
+    }
+
 }
