4ea8f37fe47cd5624f6bf0a6534df4bf8121eeaa
Siklós Magdolna <smnati@freemail.hu>
2021-10-22 13:27:26 +0200

    Finished Oszthatosag17tel.java

--- src/main/java/hu/puzzles/task/Oszthatosag17tel.java
+++ src/main/java/hu/puzzles/task/Oszthatosag17tel.java
@@ -9,9 +9,55 @@ import java.util.List;
  */
 public class Oszthatosag17tel {
 
+  public  String kozbulsoSzamok = "";
+
     public List<String> run(List<String> bemenet) {
         List<String> kimenet = new ArrayList<>();
-
+        String bemenetiSzam = bemenet.get(0);
+        if(Integer.parseInt(bemenetiSzam)%17 ==0){
+            kimenet.add("IGEN");
+        } else {
+            kimenet.add("NEM");
+        }
+        osztas17(Integer.parseInt(bemenet.get(0)));
+        kimenet.add(kozbulsoSzamok);
         return kimenet;
     }
+
+
+    public void osztas17(int bemenetiSzam) {
+        int vegeSzam = bemenetiSzam % 10;
+        int elejeSzam = bemenetiSzam / 10;
+        int ujSzam = elejeSzam - (vegeSzam) * 5;
+        if(ujSzam>=0) {
+            kozbulsoSzamok += ujSzam + " ";
+        }
+
+        if(ujSzam<=0){
+            if(kozbulsoSzamok.length()>0 )    {
+                kozbulsoSzamok = kozbulsoSzamok.substring(0,kozbulsoSzamok.length()-1);
+            }
+            return;
+
+    }
+        osztas17(ujSzam);
+
+
+
+    }
+
+
+
+    public int kivonas (int ertek){
+        int visszaSzam = -- ertek;
+        if (visszaSzam <= 0){
+            return visszaSzam;
+        }
+        return kivonas(visszaSzam);
+    }
+
+
+
+
+
 }
